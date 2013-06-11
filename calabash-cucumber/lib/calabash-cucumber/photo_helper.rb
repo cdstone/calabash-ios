9f99f1e3bc34698dec86172ae268b9bf6e4ecb01
iplukens <iplukens@wpi.edu>
2013-06-11 10:04:02 -0400

    Added method for adding just album and counting photos in an album; edited default add photo path to not include album in the JSON query

--- calabash-cucumber/lib/calabash-cucumber/photo_helper.rb
+++ calabash-cucumber/lib/calabash-cucumber/photo_helper.rb
@@ -8,14 +8,44 @@ def base64_encode(str)
     @out = Base64.encode64(str.to_s).gsub(/\n/, '') # encodes then strips new line to match php style
 end
 
+def count_photos(album)
+    res = http({:method => :post, :path => 'count'},
+               {:album => album})
+    res = JSON.parse(res)
+    if res['outcome'] != 'SUCCESS'
+        msg = "Count failed because: #{res['reason']}\n#{res['details']}"
+        raise msg
+        else
+        msg = "#res['count']}\n"
+        raise msg
+    end
+end
+
+def add_album(album)
+    res = http({:method => :post, :path => 'photo'},
+               {:album => album})
+    res = JSON.parse(res)
+    if res['outcome'] != 'SUCCESS'
+        msg = "Album creation failed because: #{res['reason']}\n#{res['details']}"
+        raise msg
+    end
+end
+
 def add_photo(dir)
     add_photo(dir, nil)
 end
 
 def add_photo(dir, album)
     image = base64_encode(File.read(dir))
-    res = http({:method => :post, :path => 'photo'},
-               {:phto => image, album => album})
+    
+    if album != nil
+        res = http({:method => :post, :path => 'photo'},
+                   {:phto => image, :album => album})
+        else
+        res = http({:method => :post, :path => 'photo'},
+                   {:phto => image})
+    end
+    
     res = JSON.parse(res)
     if res['outcome'] != 'SUCCESS'
         msg = "Image failed because: #{res['reason']}\n#{res['details']}"
