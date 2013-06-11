8a6a251e5d1844e30d79985b9dbf770f8d4d7140
iplukens <iplukens@wpi.edu>
2013-06-11 12:05:59 -0400

    fixed includes and cleaned up the count photos, add album, and add photo queries

--- calabash-cucumber/lib/calabash-cucumber/photo_helper.rb
+++ calabash-cucumber/lib/calabash-cucumber/photo_helper.rb
@@ -1,54 +1,59 @@
 require 'calabash-cucumber/tests_helpers'
 require 'base64'
 
-include Calabash::Cucumber::Core
-include Calabash::Cucumber::TestsHelpers
-
-def base64_encode(str)
-    @out = Base64.encode64(str.to_s).gsub(/\n/, '') # encodes then strips new line to match php style
-end
-
-def count_photos(album)
-    res = http({:method => :post, :path => 'count'},
-               {:album => album})
-    res = JSON.parse(res)
-    if res['outcome'] != 'SUCCESS'
-        msg = "Count failed because: #{res['reason']}\n#{res['details']}"
-        raise msg
-        else
-        msg = "#res['count']}\n"
-        puts msg
-    end
-end
-
-def add_album(album)
-    res = http({:method => :post, :path => 'photo'},
-               {:album => album})
-    res = JSON.parse(res)
-    if res['outcome'] != 'SUCCESS'
-        msg = "Album creation failed because: #{res['reason']}\n#{res['details']}"
-        raise msg
-    end
-end
-
-def add_photo(dir)
-    add_photo(dir, nil)
-end
-
-def add_photo(dir, album)
-    image = base64_encode(File.read(dir))
-    
-    if album != nil
-        res = http({:method => :post, :path => 'photo'},
-                   {:phto => image, :album => album})
-        else
-        res = http({:method => :post, :path => 'photo'},
-                   {:phto => image})
-    end
-    
-    res = JSON.parse(res)
-    if res['outcome'] != 'SUCCESS'
-        msg = "Image failed because: #{res['reason']}\n#{res['details']}"
-        raise msg
+module Calabash
+    module Cucumber
+        module PhotoHelpers
+            include Calabash::Cucumber::Core
+            include Calabash::Cucumber::TestsHelpers
+            
+            def base64_encode(str)
+                @out = Base64.encode64(str.to_s).gsub(/\n/, '') # encodes then strips new line to match php style
+            end
+            
+            def count_photos(album)
+                res = http({:method => :post, :path => 'count'},
+                           {:album => album})
+                res = JSON.parse(res)
+                if res['outcome'] != 'SUCCESS'
+                    msg = "Count failed because: #{res['reason']}\n#{res['details']}"
+                    raise msg
+                    else
+                    msg = "#res['count']}\n"
+                end
+                res['count']
+            end
+            
+            def add_album(album)
+                res = http({:method => :post, :path => 'photo'},
+                           {:album => album})
+                res = JSON.parse(res)
+                if res['outcome'] != 'SUCCESS'
+                    msg = "Album creation failed because: #{res['reason']}\n#{res['details']}"
+                    raise msg
+                end
+                res['results']
+            end
+            
+            def add_photo(dir, album)
+                image = base64_encode(File.read(dir))
+                
+                if album != nil
+                    res = http({:method => :post, :path => 'photo'},
+                               {:phto => image, :album => album})
+                    else
+                    res = http({:method => :post, :path => 'photo'},
+                               {:phto => image})
+                end
+                
+                res = JSON.parse(res)
+                if res['outcome'] != 'SUCCESS'
+                    msg = "Image failed because: #{res['reason']}\n#{res['details']}"
+                    raise msg
+                end
+                res['results']
+            end
+            
+        end
     end
 end
